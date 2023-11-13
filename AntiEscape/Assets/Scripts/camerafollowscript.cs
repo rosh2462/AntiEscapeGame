de107935bc27a54195cbc061fc6b727bbf541ee0
rosh2462 <73791456+rosh2462@users.noreply.github.com>
2023-11-13 14:31:21 +0100

    room1_updated_hover_and_new backgrounds

--- AntiEscape/Assets/Scripts/camerafollowscript.cs
+++ AntiEscape/Assets/Scripts/camerafollowscript.cs
@@ -6,29 +6,36 @@ public class camerafollowscript : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float hoverLimit = 2f; 
+    public float edgePanSpeed = 5f; 
     private Vector3 initialPosition;
-    private Vector3 lastCursorPosition;
     void Start()
     {
         initialPosition = transform.position;
-        lastCursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
     void Update()
     {
         Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cursorPos.y = initialPosition.y; 
-        cursorPos.z = initialPosition.z;
-        float cursorDistance = Vector3.Distance(cursorPos, lastCursorPosition);
-        if (cursorDistance > 0.01f)
+        cursorPos.y = initialPosition.y;
+        cursorPos.z = initialPosition.z; 
+        float distanceToCursor = Mathf.Abs(cursorPos.x - initialPosition.x);
+        if (distanceToCursor > hoverLimit)
         {
-            float distanceToCursor = Mathf.Abs(cursorPos.x - initialPosition.x);
-            if (distanceToCursor > hoverLimit)
-            {
-                cursorPos.x = initialPosition.x + Mathf.Sign(cursorPos.x - initialPosition.x) * hoverLimit;
-            }
-
+            cursorPos.x = initialPosition.x + Mathf.Sign(cursorPos.x - initialPosition.x) * hoverLimit;
+        }
+        if (IsCursorAtScreenEdge())
+        {
             transform.position = Vector3.Lerp(transform.position, cursorPos, moveSpeed * Time.deltaTime);
         }
-        lastCursorPosition = cursorPos;
+    }
+    bool IsCursorAtScreenEdge()
+    {
+        float edgeThreshold = 20f; 
+        Vector3 mousePos = Input.mousePosition;
+        bool atEdge = false;
+        if (mousePos.x <= edgeThreshold || mousePos.x >= Screen.width - edgeThreshold)
+        {
+            atEdge = true;
+        }
+        return atEdge;
     }
 }
