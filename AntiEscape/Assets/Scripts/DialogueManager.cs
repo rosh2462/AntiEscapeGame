b16c6a18c0bebfe14d1a96310f497c1b4373aa5d
PoojaPrasad12 <poojaprasad.9898@gmail.com>
2023-11-22 02:06:15 +0100

    characters added for room 1 dialogues

--- AntiEscape/Assets/Scripts/DialogueManager.cs
+++ AntiEscape/Assets/Scripts/DialogueManager.cs
@@ -8,20 +8,52 @@ using Yarn.Unity;
 public class DialogueManager : MonoBehaviour
 {  
     
-     public SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component
+    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRendererAnimal;
 
     public Sprite door1crate;
+    public Sprite emotionnanditha;
+    public Sprite emotionhugh;
+    public Sprite emotionmathilda;
+    public Sprite emotionmarzanna;
+    public Sprite trap1animal;
+
   //  public Sprite extra1scroll;
-  
+
+    private Sprite[] characters; // Array to hold character sprites
+    private int currentIndex; // Index to keep track of the current character sprite
+ 
+    void Start()
+    {
+        characters = new Sprite[] { emotionnanditha, emotionhugh, emotionmathilda };
+        currentIndex = 0; // Start with the first character
+    }
 
     [YarnCommand("setsprite")]
     public void SetSprite(string expression)
     {
+
         switch (expression)
         {
+             
             case "door1crate_open":
                 spriteRenderer.sprite = door1crate;
                 break;
+
+            case "trap1animal_activated":
+                spriteRendererAnimal.sprite = trap1animal;
+                break;
+
+            case "idle":
+               
+                spriteRenderer.sprite = characters[currentIndex];
+                currentIndex = (currentIndex + 1) % characters.Length;              
+                break;
+
+            case "scared":
+                spriteRenderer.sprite = emotionmarzanna;
+                break;
+
             // case "extra1scroll_idle":
             //     spriteRenderer.sprite = extra1scroll;
             //     break;
