5e0704758fcf59398eb69d818b229049397fb0d2
PoojaPrasad12 <poojaprasad.9898@gmail.com>
2023-12-19 21:15:50 +0100

    character script changed

--- AntiEscape/Assets/Scripts/Character.cs
+++ AntiEscape/Assets/Scripts/Character.cs
@@ -25,6 +25,13 @@ public class Character : MonoBehaviour
     public SpriteRenderer spriteRenderer19; 
     public SpriteRenderer spriteRenderer20;
     public SpriteRenderer spriteRenderer21;
+    public SpriteRenderer spriteRenderer22;
+    public SpriteRenderer spriteRenderer23;
+    public SpriteRenderer spriteRenderer24;
+    public SpriteRenderer spriteRenderer25;
+    public SpriteRenderer spriteRenderer26;
+    public SpriteRenderer spriteRenderer27;
+    public SpriteRenderer spriteRenderer28;
 
     public Sprite emotionnanditha_idle;
     public Sprite emotionhugh_idle;
@@ -42,6 +49,7 @@ public class Character : MonoBehaviour
     public Sprite emotionhugh_annoyed;
     public Sprite emotionnanditha_annoyed;
     public Sprite emotionmathilda_annoyed;
+    public Sprite emotionmarzanna_annoyed;
 
     public Sprite emotionhugh_afraid;
     public Sprite emotionnanditha_afraid;
@@ -52,6 +60,15 @@ public class Character : MonoBehaviour
     public Sprite emotionmarzanna_sad;
     public Sprite emotionnanditha_sad;
     public Sprite emotionhugh_sad;
+    public Sprite emotionashok_sad;
+    public Sprite emotionmathilda_sad;
+
+    public Sprite emotionmarzanna_embarassed;
+    public Sprite emotionashok_embarassed;
+
+    public Sprite emotionhugh_angry;
+
+    public Sprite emotionmathilda_scream;
 
     void Start() {
         DisableAllSprites();
@@ -154,6 +171,36 @@ public class Character : MonoBehaviour
                 spriteRenderer21.sprite = emotionhugh_sad;
                 spriteRenderer21.enabled = true;
                 break;
+
+
+            case "mt_sad":
+                spriteRenderer22.sprite = emotionmathilda_sad;
+                spriteRenderer22.enabled = true;
+                break;
+            case "mz_annoyed":
+                spriteRenderer23.sprite = emotionmarzanna_annoyed;
+                spriteRenderer23.enabled = true;
+                break;
+            case "a_sad":
+                spriteRenderer24.sprite = emotionashok_sad;
+                spriteRenderer24.enabled = true;
+                break;
+            case "mt_scream":
+                spriteRenderer25.sprite = emotionmathilda_scream;
+                spriteRenderer25.enabled = true;
+                break;
+            case "h_angry":
+                spriteRenderer26.sprite = emotionhugh_angry;
+                spriteRenderer26.enabled = true;
+                break;
+            case "mz_embarassed":
+                spriteRenderer27.sprite = emotionmarzanna_embarassed;
+                spriteRenderer27.enabled = true;
+                break;
+            case "a_embarassed":
+                spriteRenderer28.sprite = emotionashok_embarassed;
+                spriteRenderer28.enabled = true;
+                break;
             default:
                 Debug.LogWarning("Unknown expression: " + expression);
                 break;
@@ -185,6 +232,13 @@ public class Character : MonoBehaviour
         spriteRenderer19.enabled = false;
         spriteRenderer20.enabled = false;
         spriteRenderer21.enabled = false;
+        spriteRenderer22.enabled = false;
+        spriteRenderer23.enabled = false;
+        spriteRenderer24.enabled = false;
+        spriteRenderer25.enabled = false;
+        spriteRenderer26.enabled = false;
+        spriteRenderer27.enabled = false;
+        spriteRenderer28.enabled = false;
     }
     
     [YarnCommand("destroyemotion")]
@@ -192,53 +246,92 @@ public class Character : MonoBehaviour
     {
         switch (expression)
         {
-            case "n_idle":
+            case "n_idle":               
                 this.spriteRenderer.enabled = false;
                 break;
-            case "a_idle":
-                this.spriteRenderer4.enabled = false;
-                break;
-            case "mt_afraid":
-                this.spriteRenderer9.enabled = false;
-                break;
-            case "h_idle":
+            case "h_idle":              
                 this.spriteRenderer1.enabled = false;
                 break;
-            case "h_afraid":
-                this.spriteRenderer8.enabled = false;
+            case "mt_idle":              
+                this.spriteRenderer2.enabled = false;
                 break;
-            case "n_annoyed":
-                this.spriteRenderer17.enabled = false;
-                break;
-            case "n_happy":
-                this.spriteRenderer6.enabled = false;
-                break;
-            case "mz_sad":
-                this.spriteRenderer19.enabled = false;
-                break;
-            case "mt_annoyed":
-                this.spriteRenderer18.enabled = false;
-                break;
-           case "n_afraid":
-                this.spriteRenderer10.enabled = false;
-                break;
-            case "n_sad":
-                this.spriteRenderer20.enabled = false;
-                break;
-            case "h_sad":
-                this.spriteRenderer21.enabled = false;
-                break;
-            case "a_annoyed":
-                this.spriteRenderer7.enabled = false;
-                break;
-            case "a_happy":
-                this.spriteRenderer14.enabled = false;
-                break;
-            case "mz_idle":
+            case "mz_idle":            
                 this.spriteRenderer3.enabled = false;
                 break;
-            case "h_happy":
+            case "a_idle":           
+                this.spriteRenderer4.enabled = false;
+                break;
+            case "mt_happy":            
+                this.spriteRenderer5.enabled = false;
+                break;
+            case "n_happy":             
+                this.spriteRenderer6.enabled = false;
+                break;
+            case "a_annoyed":             
+                this.spriteRenderer7.enabled = false;
+                break;
+            case "h_afraid":             
+                this.spriteRenderer8.enabled = false;
+                break;
+            case "mt_afraid":              
+                this.spriteRenderer9.enabled = false;
+                break;
+            case "n_afraid":             
+                this.spriteRenderer10.enabled = false;
+                break;
+            case "a_afraid":            
+                this.spriteRenderer11.enabled = false;
+                break;
+            case "mz_afraid":              
+                this.spriteRenderer12.enabled = false;
+                break;
+            case "mz_happy":               
+                this.spriteRenderer13.enabled = false;
+                break;
+            case "a_happy":          
+                this.spriteRenderer14.enabled = false;
+                break;
+            case "h_happy":             
                 this.spriteRenderer15.enabled = false;
+                break;
+            case "h_annoyed":               
+                this.spriteRenderer16.enabled = false;
+                break;
+            case "n_annoyed":             
+                this.spriteRenderer17.enabled = false;
+                break;
+            case "mt_annoyed":           
+                this.spriteRenderer18.enabled = false;
+                break;
+            case "mz_sad":          
+                this.spriteRenderer19.enabled = false;
+                break;
+            case "n_sad":          
+                this.spriteRenderer20.enabled = false;
+                break;
+            case "h_sad":           
+                this.spriteRenderer21.enabled = false;
+                break;
+            case "mt_sad":     
+                this.spriteRenderer22.enabled = false;
+                break;
+            case "mz_annoyed":            
+                this.spriteRenderer23.enabled = false;
+                break;
+            case "a_sad":             
+                this.spriteRenderer24.enabled = false;
+                break;
+            case "mt_scream":              
+                this.spriteRenderer25.enabled = false;
+                break;
+            case "h_angry":                
+                this.spriteRenderer26.enabled = false;
+                break;
+            case "mz_embarassed":            
+                this.spriteRenderer27.enabled = false;
+                break;
+            case "a_embarassed":              
+                this.spriteRenderer28.enabled = false;
                 break;
             default:
                 Debug.LogWarning("Unknown expression: " + expression);
@@ -251,58 +344,96 @@ public class Character : MonoBehaviour
     {
        switch (expression)
         {
-            case "n_idle":
+            case "n_idle":               
                 this.spriteRenderer.enabled = true;
                 break;
-            case "a_idle":
-                this.spriteRenderer4.enabled = true;
-                break;
-            case "mt_afraid":
-                this.spriteRenderer9.enabled = true;
-                break;
-            case "h_idle":
+            case "h_idle":              
                 this.spriteRenderer1.enabled = true;
                 break;
-            case "h_afraid":
-                this.spriteRenderer8.enabled = true;
+            case "mt_idle":              
+                this.spriteRenderer2.enabled = true;
                 break;
-            case "n_annoyed":
-                this.spriteRenderer17.enabled = true;
-                break;
-            case "n_happy":
-                this.spriteRenderer6.enabled = true;
-                break;
-            case "mz_sad":
-                this.spriteRenderer19.enabled = true;
-                break;
-            case "mt_annoyed":
-                this.spriteRenderer18.enabled = true;
-                break;
-            case "n_afraid":
-                this.spriteRenderer10.enabled = true;
-                break;
-            case "n_sad":
-                this.spriteRenderer20.enabled = true;
-                break;
-            case "h_sad":
-                this.spriteRenderer21.enabled = true;
-                break;
-            case "a_annoyed":
-                this.spriteRenderer7.enabled = true;
-                break;
-            case "a_happy":
-                this.spriteRenderer14.enabled = true;
-                break;
-            case "mz_idle":
+            case "mz_idle":            
                 this.spriteRenderer3.enabled = true;
                 break;
-            case "h_happy":
+            case "a_idle":           
+                this.spriteRenderer4.enabled = true;
+                break;
+            case "mt_happy":            
+                this.spriteRenderer5.enabled = true;
+                break;
+            case "n_happy":             
+                this.spriteRenderer6.enabled = true;
+                break;
+            case "a_annoyed":             
+                this.spriteRenderer7.enabled = true;
+                break;
+            case "h_afraid":             
+                this.spriteRenderer8.enabled = true;
+                break;
+            case "mt_afraid":              
+                this.spriteRenderer9.enabled = true;
+                break;
+            case "n_afraid":             
+                this.spriteRenderer10.enabled = true;
+                break;
+            case "a_afraid":            
+                this.spriteRenderer11.enabled = true;
+                break;
+            case "mz_afraid":              
+                this.spriteRenderer12.enabled = true;
+                break;
+            case "mz_happy":               
+                this.spriteRenderer13.enabled = true;
+                break;
+            case "a_happy":          
+                this.spriteRenderer14.enabled = true;
+                break;
+            case "h_happy":             
                 this.spriteRenderer15.enabled = true;
+                break;
+            case "h_annoyed":               
+                this.spriteRenderer16.enabled = true;
+                break;
+            case "n_annoyed":             
+                this.spriteRenderer17.enabled = true;
+                break;
+            case "mt_annoyed":           
+                this.spriteRenderer18.enabled = true;
+                break;
+            case "mz_sad":          
+                this.spriteRenderer19.enabled = true;
+                break;
+            case "n_sad":          
+                this.spriteRenderer20.enabled = true;
+                break;
+            case "h_sad":           
+                this.spriteRenderer21.enabled = true;
+                break;
+            case "mt_sad":     
+                this.spriteRenderer22.enabled = true;
+                break;
+            case "mz_annoyed":            
+                this.spriteRenderer23.enabled = true;
+                break;
+            case "a_sad":             
+                this.spriteRenderer24.enabled = true;
+                break;
+            case "mt_scream":              
+                this.spriteRenderer25.enabled = true;
+                break;
+            case "h_angry":                
+                this.spriteRenderer26.enabled = true;
+                break;
+            case "mz_embarassed":            
+                this.spriteRenderer27.enabled = true;
+                break;
+            case "a_embarassed":              
+                this.spriteRenderer28.enabled = true;
                 break;
             default:
                 Debug.LogWarning("Unknown expression: " + expression);
                 break;
         }
     }
-// testetstetst
 }
